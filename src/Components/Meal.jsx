ab57d95ec2403ad1fb6f56bf178e63136b015e82
Andry Pribadi <50471550+AndryPribadi@users.noreply.github.com>
2022-11-25 19:09:33 +0800

    meal

---
+++ src/Components/Meal.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Meal = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="heading">
+          <h1>Search Food Recipe</h1>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+            assumenda officiis rem tempore minus pariatur, nostrum
+            exercitationem possimus corporis sunt asperiores repellendus
+            repudiandae voluptas debitis consectetur odio iure delectus iste.
+          </h4>
+        </div>
+        <div className="searchBox">
+          <input type="search" className="search-bar" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Meal;
