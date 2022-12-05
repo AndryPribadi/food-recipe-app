2d4b52cefb81a9deaa08427df70e94277a835ac8
Andry Pribadi <50471550+AndryPribadi@users.noreply.github.com>
2022-12-05 12:06:32 +0800

    api recipeinfo

--- src/Components/RecipeInfo.jsx
+++ src/Components/RecipeInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
+let vId = "";
 const RecipeInfo = () => {
   const [item, setItem] = useState();
   const { MealId } = useParams();
@@ -11,6 +12,11 @@ const RecipeInfo = () => {
       .then((data) => {
         setItem(data.meals[0]);
       });
+  }
+  if (item) {
+    const url = item.strYoutube;
+    const str = url.split("=");
+    vId = str[str.length - 1];
   }
   return (
     <>
@@ -24,6 +30,30 @@ const RecipeInfo = () => {
               <h1>{item.strMeal}</h1>
               <h2>{item.strArea} Food</h2>
               <h3>Category {item.strCategory}</h3>
+            </div>
+          </div>
+          <div className="recipe-details">
+            <div className="ingredients">
+              <h2>Ingredients</h2>
+              <br />
+              <h4>
+                {item.strIngredient1}:{item.strMeasure1}
+                {item.strIngredient2}:{item.strMeasure2}
+                {item.strIngredient3}:{item.strMeasure3}
+                {item.strIngredient4}:{item.strMeasure4}
+                {item.strIngredient5}:{item.strMeasure5}
+                {item.strIngredient6}:{item.strMeasure6}
+                {item.strIngredient7}:{item.strMeasure7}
+                {item.strIngredient8}:{item.strMeasure8}
+              </h4>
+            </div>
+            <div className="instructions">
+              <h2>Instructions</h2>
+              <br />
+              <h4>{item.strInstructions}</h4>
+            </div>
+            <div className="video">
+              <iframe src={`https://www.youtube.com/embed/${vId}`}></iframe>
             </div>
           </div>
         </>
